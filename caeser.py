01f286b746e9e7db73e04d8a7b7df588162e1f46
Andy MacDonad <wamacdonald89@gmail.com>
2015-11-17 22:36:47 -0500

    Small changes

--- caeser.py
+++ caeser.py
@@ -10,7 +10,7 @@ def decrypt(cipher, shift=None):
         for i in range(0, 26):
             ciphers.append(decrypt(cipher, i))
         scores = score_fitness(ciphers)
-        print ciphers[scores.index(max(scores))]
+        return ciphers[scores.index(max(scores))]
     else:
         return ''.join([chr(((ord(x)-65-shift) % 26) + 65) for x in cipher.upper() if x in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'])
 
--- railfence.py
+++ railfence.py
@@ -1,4 +1,4 @@
-from utility import normalize
+from utility import normalize, score_fitness
 from itertools import cycle
 
 def encrypt(plaintext,n):
@@ -10,20 +10,27 @@ def encrypt(plaintext,n):
         rails[index] = rails[index] + i
     return ''.join(rails)
 
-def decrypt(cipher,n):
-    cipherlen = len(cipher)
-    parts = [''] * n
-    count = cycle(range(0,n) + range(1,n-1)[::-1])
-    for i in cipher:
-        index = count.next()
-        parts[index] += 'x'
-    for i in range(0,n):
-        parts[i] = cipher[:len(parts[i])]
-        cipher = cipher[len(parts[i]):]
-    count = cycle(range(0,n) + range(1,n-1)[::-1])
-    plaintext = ''
-    for i in range(0, cipherlen):
-        index = count.next()
-        plaintext += parts[index][0]
-        parts[index] = parts[index][1:]
-    return plaintext
+def decrypt(cipher,n=None):
+    if n == None:
+        ciphers = []
+        for i in range(1, len(cipher)):
+            ciphers.append(decrypt(cipher, i))
+        scores = score_fitness(ciphers)
+        return ciphers[scores.index(max(scores))]
+    else:
+        cipherlen = len(cipher)
+        parts = [''] * n
+        count = cycle(range(0,n) + range(1,n-1)[::-1])
+        for i in cipher:
+            index = count.next()
+            parts[index] += 'x'
+        for i in range(0,n):
+            parts[i] = cipher[:len(parts[i])]
+            cipher = cipher[len(parts[i]):]
+        count = cycle(range(0,n) + range(1,n-1)[::-1])
+        plaintext = ''
+        for i in range(0, cipherlen):
+            index = count.next()
+            plaintext += parts[index][0]
+            parts[index] = parts[index][1:]
+        return plaintext
